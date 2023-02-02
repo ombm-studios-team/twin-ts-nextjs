cce8c60d81efdf2905df610f32466b88568c776f
Che' J. Holloway <chejholloway@protonmail.com>
2023-02-02 09:51:53 -0800

    test(global): :sparkles: adds & configures jest
    tw-styled components still not passing tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,19 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'prettier', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
+    'plugin:storybook/recommended'
+  ],
   plugins: ['react', '@typescript-eslint', 'prettier'],
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
+    jest: true
   },
+  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -15,23 +23,31 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
+    '@typescript-eslint/no-var-requires': 0,
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     'react/jsx-props-no-spreading': 'off',
     'import/prefer-default-export': 'off',
     'no-param-reassign': 'off',
-    'import/extensions': ['error', 'ignorePackages', {
-      ts: 'never',
-      tsx: 'never'
-    }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
     'consistent-return': 'off',
     'arrow-body-style': 'off',
     'prefer-arrow-callback': 'off',
     'react/jsx-filename-extension': 'off',
-    'react/function-component-definition': ['error', {
-      namedComponents: 'arrow-function',
-      unnamedComponents: 'arrow-function'
-    }],
-    'prettier/prettier': 'warn'
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function'
+      }
+    ],
+    'prettier/prettier': ['warn', { endOfLine: 'auto' }]
   }
 };
