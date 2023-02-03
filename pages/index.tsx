d50e9e1d073e93cdb2dbd005d86aabe3fde515e6
Che' J. Holloway <chejholloway@protonmail.com>
2023-02-02 21:01:22 -0800

    fix(global): :building_construction: get build command to work

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const styles = {
   // Move long class sets out of jsx to keep it scannable
   container: ({ hasBackground }: { hasBackground: boolean }) => [
     tw`flex flex-col items-center justify-center h-screen`,
-    hasBackground && tw`bg-gradient-to-b from-electric to-ribbon`
+    hasBackground && tw`bg-gradient-to-b from-green-400 to-ribbon`
   ]
 };
 
