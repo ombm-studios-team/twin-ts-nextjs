c445ade0169a5c6fd1ffdd8d0e239bdde6c57f23
Che' J. Holloway <chejholloway@protonmail.com>
2023-02-10 18:16:31 -0800

    refactor(global): :art: a good stopping point

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,97 @@
 import tw from 'twin.macro';
-import { Button } from '../components';
+import { Table } from '../components';
+
+const getShortName = fullName => {
+  return fullName
+    .split(' ')
+    .map(n => n[0])
+    .join('');
+};
+
+const getData = () => [
+  {
+    name: 'Jerome Bell',
+    isCovered: true,
+    isSubscriber: true,
+    insurance: 'Primary',
+    id: ''
+  },
+  {
+    name: 'Stacy Bell',
+    isCovered: true,
+    isSubscriber: false,
+    insurance: 'Primary',
+    id: ''
+  },
+  {
+    name: 'Rebecca Bell',
+    isCovered: false,
+    isSubscriber: false,
+    insurance: '',
+    id: ''
+  }
+];
 
 const styles = {
-  // Move long class sets out of jsx to keep it scannable
-  container: ({ hasBackground }: { hasBackground: boolean }) => [
-    tw`flex flex-col items-center justify-center h-screen`,
-    hasBackground && tw`bg-gradient-to-b from-green-400 to-ribbon`
-  ]
+  container: () => [tw`flex flex-col p-8`],
+  checkboxes: () => [tw`text-left pl-8`]
+};
+
+const makeColumns = () => {
+  return [
+    {
+      Header: 'Covered 🛈',
+      accessor: 'isCovered',
+      Cell: row => {
+        return (
+          <div css={styles.checkboxes()}>
+            <input
+              type="checkbox"
+              defaultChecked={row.value == true ? true : false}
+            />
+          </div>
+        );
+      }
+    },
+    {
+      Header: 'Name',
+      accessor: 'name'
+    },
+    {
+      Header: 'Subscriber',
+      accessor: 'isSubscribed'
+    },
+    {
+      Header: 'Insurance',
+      accessor: 'insurance',
+      Cell: row => {
+        return (
+          <div className="relative w-full lg:max-w-sm">
+            <select
+              defaultChecked={row.value != '' ? 'Primary' : 'Seconday'}
+              className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+            >
+              <option>Primary</option>
+              <option>Secondary</option>
+              <option defaultValue hidden></option>
+            </select>
+          </div>
+        );
+      }
+    },
+    {
+      Header: 'ID',
+      accessor: 'id'
+    }
+  ];
 };
 
 const App = () => (
-  <div css={styles.container({ hasBackground: true })}>
-    <div tw="flex flex-col justify-center h-full gap-y-5">
-      <Button variant="primary">Submit</Button>
-      <Button variant="secondary">Cancel</Button>
-      <Button isSmall>Close</Button>
+  <>
+    <div css={styles.container()}>
+      <Table columns={makeColumns()} data={getData()} />
     </div>
-  </div>
+  </>
 );
 
 export default App;
